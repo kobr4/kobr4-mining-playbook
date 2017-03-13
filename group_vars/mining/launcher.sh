2e4d8d36180e38a258e66cd82f8cd8513bf93646
kobr4 <nicolas.my@gmail.com>
2017-03-13 23:04:55 +0100

    Added cron reboot

--- group_vars/mining/launcher.sh
+++ group_vars/mining/launcher.sh
@@ -5,8 +5,8 @@
 function d_start()
 {
 	echo "Claymore : starting service"
-#	/home/kobr4/claymore/ethdcrminer64 -epool http://eth1.nanopool.org:8888/0x615dd04F4F9e6789fb4FdB2A1123935A5421ef0B  & 
-        /home/kobr4/claymore/ethdcrminer64 -epool pool.alpereum.ch:3001 -ewal 0x615dd04F4F9e6789fb4FdB2A1123935A5421ef0B.MINING1 &
+	/home/kobr4/claymore/ethdcrminer64 -epool http://eth1.nanopool.org:8888/0x615dd04F4F9e6789fb4FdB2A1123935A5421ef0B  & 
+#        /home/kobr4/claymore/ethdcrminer64 -epool pool.alpereum.ch:3001 -ewal 0x615dd04F4F9e6789fb4FdB2A1123935A5421ef0B.MINING1 &
         export APP_PID=$!
 	echo $APP_PID > /tmp/claymore.pid
         sleep 5
